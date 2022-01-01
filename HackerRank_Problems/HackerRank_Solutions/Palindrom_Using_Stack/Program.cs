61cbe2cd7e273afdef99e8106910b16fbe811a65
Ashraf Ali <60466741+AshrafAli02@users.noreply.github.com>
2022-01-01 16:34:50 +0530

    Stack And Queue

---
+++ HackerRank_Problems/HackerRank_Solutions/Palindrom_Using_Stack/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom_Using_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            string word = "asdsaa";
+
+            foreach(char c in word)
+            {
+                obj.pushCharacter(c);
+                obj.enqueChaacter(c);
+            }
+
+            bool IsPalindrom = true;
+            for(int i=0;i<word.Length;i++)
+            {
+                if(obj.popCharacter()!=obj.dequeueCharacter())
+                {
+                    IsPalindrom = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(IsPalindrom);
+            Console.ReadKey();
+        }
+
+        Stack<char> WordStack = new Stack<char>();
+        Queue<char> WordQueue = new Queue<char>();
+
+
+        public void pushCharacter(char c)
+        {
+            WordStack.Push(c);
+        }
+        public void enqueChaacter(char c)
+        {
+            WordQueue.Enqueue(c);
+        }
+
+        public char popCharacter()
+        {
+            return WordStack.Pop();
+        }
+        public char dequeueCharacter()
+        {
+            return WordQueue.Dequeue();
+        }
+    }
+}
---
+++ HackerRank_Problems/HackerRank_Solutions/StringToInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            try
+            {
+                int Number = Convert.ToInt32(S);
+                Console.WriteLine(Number);
+            }
+            catch
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+
+
+    }
+}
