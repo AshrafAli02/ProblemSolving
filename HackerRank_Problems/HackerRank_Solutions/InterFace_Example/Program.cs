c870073c294a53455088f4a1015f1ca40cc0c9c5
Ashraf Ali <60466741+AshrafAli02@users.noreply.github.com>
2022-01-04 07:34:36 +0530

    interface_Sorting

---
+++ HackerRank_Problems/HackerRank_Solutions/InterFace_Example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFace_Example
+{
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+    }
+
+    public class Calculator : AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            int Sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Sum += i;
+                }
+            }
+
+            return Sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+}
---
+++ HackerRank_Problems/HackerRank_Solutions/Sorting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            // Write your code here
+            int NoOfSwaps = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                int Swaps = 0;
+                for (int j = i + 1; j < a.Count; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        int temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                        Swaps++;
+                        NoOfSwaps++;
+                    }
+                }
+                if (Swaps == 0) { break; }
+
+            }
+
+            Console.WriteLine("Array is sorted in " + NoOfSwaps + " swaps.");
+            Console.WriteLine("First Element: " + a[0]);
+            Console.WriteLine("Last Element: " + a[a.Count - 1]);
+        }
+    }
+}
